0ab84f3b81eeebd1879c5a4460d1890c36fd74f3
sharmarke <shar.alternatif@gmail.com>
2022-08-20 16:51:18 +0300

    Create add, multiply, divide and substract functions

---
+++ script.js
@@ -0,0 +1,16 @@
+function add(a, b){
+    return a + b;
+}
+
+function substract(a, b){
+    return a - b
+}
+
+function multiply(a, b){
+    return a * b
+}
+
+function divide(a, b){
+    if(b === 0) return 'Error: Division by 0'
+    return a / b
+}
