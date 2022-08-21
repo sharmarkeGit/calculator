15c3b6b8c2aa9843f0e73e6d5fccc7ffdd41bb5e
sharmarke <shar.alternatif@gmail.com>
2022-08-21 15:27:35 +0300

    Calculate + / and * This is a first functional version. I will add +/- and %. I will also add the possibility to keep pressing '=' in order to keep adding (or subtracting...) the second operand after an operation...

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let value = null;
+
 function add(a, b){
     return a + b;
 }
@@ -21,6 +23,89 @@ function operate(operator, a, b){
         case "+" : return add(a, b)
         case "-" : return subtract(a, b)
         case "/" : return divide(a, b)
-        case "*" : return multiply(a, b)
+        case "x" : return multiply(a, b)
     }
 }
+
+
+// Adding listeners to the calculator's buttons
+const items = document.querySelectorAll('.grid-items')
+items.forEach(item => {
+    item.addEventListener('click',display);
+        
+});
+
+function display(e){
+
+    const currentKey = e.target.textContent;
+    if(currentKey === 'C'){
+        clearData();
+        return;
+    };
+    const screen = document.querySelector('.screen');
+    
+
+
+    //if the key is a number
+    isKeyNum = Number.isInteger(parseInt(currentKey))
+
+
+    
+    if(isKeyNum && keyOperator === null && keyB === null){
+        if(keyA === null) keyA = currentKey
+        else keyA += currentKey 
+        screen.textContent += currentKey
+    }else if(!isKeyNum && keyA === null && keyB === null){
+        keyA = 0
+        keyOperator = currentKey
+    }else if(!isKeyNum && keyB === null){
+        keyOperator = currentKey
+    }else if(!isKeyNum && keyB !==null && currentKey !== '='){
+        clearScreen();
+        const lastResult = operate(keyOperator, parseInt(keyA), parseInt(keyB))
+        screen.textContent = lastResult
+        keyA = lastResult
+        keyOperator = currentKey
+        keyB = null
+        
+    }else if(!isKeyNum && keyB !==null && currentKey === '='){
+        clearScreen();
+        const lastResult = operate(keyOperator, parseInt(keyA), parseInt(keyB))
+        screen.textContent = lastResult
+        keyA = lastResult
+        keyOperator = null
+        keyB = null
+        
+    }else if (keyB === null){
+        clearScreen();
+        keyB = currentKey;
+        screen.textContent += currentKey
+     }else if (keyB !==null){
+         keyB += currentKey;
+         screen.textContent += currentKey
+     }
+    
+
+}
+
+function clearData(){
+    const screen = document.querySelector('.screen');
+    screen.textContent = ''
+    keyA = null
+    keyB = null
+    keyOperator = null
+}
+
+function clearScreen(){
+    const screen = document.querySelector('.screen');
+    screen.textContent = ''
+
+}
+
+
+
+
+let keyA = null
+let keyB = null
+let keyOperator = null
+
