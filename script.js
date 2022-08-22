c507ba87563bb473ac190369f8f72bf716df585c
sharmarke <shar.alternatif@gmail.com>
2022-08-22 15:45:42 +0300

    Refactor working calculator

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-let value = null;
+let a = 0
+let b = null
+let lastKeyOperator = null
+let keyOperator = null
+let screen = document.querySelector('.screen');
 
 function add(a, b){
     return a + b;
@@ -28,84 +32,149 @@ function operate(operator, a, b){
 }
 
 
-// Adding listeners to the calculator's buttons
-const items = document.querySelectorAll('.grid-items')
-items.forEach(item => {
-    item.addEventListener('click',display);
+// Adding listeners to the calculator's number buttons 
+const btnNumbers = document.querySelectorAll('.number')
+btnNumbers.forEach(btn => {
+    btn.addEventListener('click',setOperands);
         
 });
 
-function display(e){
-
-    const currentKey = e.target.textContent;
-    if(currentKey === 'C'){
-        clearData();
-        return;
-    };
-    const screen = document.querySelector('.screen');
-    
-
-
-    //if the key is a number
-    isKeyNum = Number.isInteger(parseInt(currentKey))
-
-
-    
-    if(isKeyNum && keyOperator === null && keyB === null){
-        if(keyA === null) keyA = currentKey
-        else keyA += currentKey 
-        screen.textContent += currentKey
-    }else if(!isKeyNum && keyA === null && keyB === null){
-        keyA = 0
-        keyOperator = currentKey
-    }else if(!isKeyNum && keyB === null){
-        keyOperator = currentKey
-    }else if(!isKeyNum && keyB !==null && currentKey !== '='){
-        clearScreen();
-        const lastResult = operate(keyOperator, parseInt(keyA), parseInt(keyB))
-        screen.textContent = lastResult
-        keyA = lastResult
-        keyOperator = currentKey
-        keyB = null
+// Adding listeners to the calculator's operator buttons
+const btnOperators = document.querySelectorAll('.operator')
+btnOperators.forEach(btn => {
+    btn.addEventListener('click',operatorClicked);
         
-    }else if(!isKeyNum && keyB !==null && currentKey === '='){
-        clearScreen();
-        const lastResult = operate(keyOperator, parseInt(keyA), parseInt(keyB))
-        screen.textContent = lastResult
-        keyA = lastResult
+});
+
+// Adding listeners to the calculator's '=' button
+const btnEqual = document.querySelectorAll('#equal')
+btnEqual.forEach(btn => {
+    btn.addEventListener('click',calculate);
+        
+});
+
+
+// Adding listeners to the calculator's 'C' button (clear)
+const btnClear = document.querySelectorAll('#clear')
+btnClear.forEach(btn => {
+    btn.addEventListener('click',clearScreen);
+        
+});
+
+
+// Adding listeners to the calculator's '+/-' button
+const btnSign = document.querySelectorAll('#changeSign')
+btnSign.forEach(btn => {
+    btn.addEventListener('click',changeSign);
+        
+});
+
+// Adding listeners to the calculator's '%' button
+const btnPercent = document.querySelectorAll('#percentage')
+btnPercent.forEach(btn => {
+    btn.addEventListener('click',percentage);
+        
+});
+
+// Adding listeners to the calculator's ',' button
+const btnFloat = document.querySelectorAll('#floater')
+btnFloat.forEach(btn => {
+    btn.addEventListener('click',floater);
+        
+});
+
+
+function display(string){
+    if(typeof string !== 'string') string = string.toString()
+    
+    if(string.length <= 18) screen.textContent = string
+   
+    else if (b===null) a = a.toString().slice(0,-1)
+    else b = b.toString().slice(0,-1)
+
+}
+
+
+function clearScreen(e){
+    
+    if(b === null && keyOperator === null){
+        screen.textContent = ''
+        a = 0
+    }else if(b === null && keyOperator !== null){
         keyOperator = null
-        keyB = null
-        
-    }else if (keyB === null){
-        clearScreen();
-        keyB = currentKey;
-        screen.textContent += currentKey
-     }else if (keyB !==null){
-         keyB += currentKey;
-         screen.textContent += currentKey
-     }
+    }else{
+        screen.textContent = ''
+        b = null
+    }
+
     
 
 }
 
-function clearData(){
-    const screen = document.querySelector('.screen');
-    screen.textContent = ''
-    keyA = null
-    keyB = null
-    keyOperator = null
+function changeSign(e){
+    if(b === null){
+        a *= -1 
+        display(a)
+    }else{
+        b *= -1 
+        display(b)
+    }
 }
 
-function clearScreen(){
-    const screen = document.querySelector('.screen');
-    screen.textContent = ''
+function percentage(e){
+    if(b === null){
+         a = parseFloat(a)/100 
+        display(a)
+    }else{
+        b = parseFloat(b) / 100
+        display(b)
+    }
+}
+
+function floater(e){
+    console.log('heere')
+    if(b === null){
+        a += '.' 
+        display(a)
+    }else{
+        b += '.'
+        display(b)
+    }
 
 }
 
+function setOperands(e){
+    const currentKey = e.target.textContent
+    if(keyOperator === null){
+        a = (a === 0) ? currentKey : a + currentKey
+        display(a)
+    }else{
+        b = (b === null) ? currentKey : b + currentKey
+        display(b)
+    }
+}
 
 
+function operatorClicked(e){
+    const currentKey = e.target.textContent
+    if(b === null){
+        keyOperator = currentKey
+    }else{
+        a = operate(keyOperator, parseFloat(a), parseFloat(b))
+        display(a)
+        b = null
+        keyOperator = currentKey
+    }
+}
 
-let keyA = null
-let keyB = null
-let keyOperator = null
-
+function calculate(e){
+    
+    if(b===null){
+        a = screen.textContent
+    }else{
+        a = operate(keyOperator,parseFloat(a),parseFloat(b))
+        screen.textContent = a
+        b = null
+        keyOperator = null
+    }
+}
